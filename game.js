d8e6760e514b337720bdaff1ea6c8c9f48bc6e4e
gabiusp <sidereal@usp.br>
2021-08-21 20:27:19 -0300

    organizando as coisas pq sou corna !

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ function makePopUp(map, text, isPuzzle) {
 
     var container = document.createElement('div');
     container.setAttribute("class", "container");
-    container.style.backgroundImage = "url('pausa.png')";
+    container.style.backgroundImage = "url('assets/img/pausa.png')";
 
     var h1 = document.createElement('h1');
     h1.innerHTML = text
