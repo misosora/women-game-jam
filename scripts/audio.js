d9f252d00467a5f741cccfc2371ffef1faa7fde3
milenacsilva <milenacorreasilva@usp.br>
2021-08-22 20:19:20 -0300

    add sound buttons

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,5 +1,16 @@
 function playAudio() {
-    let audio = document.querySelector('audio')
-    audio.loop = true
-    audio.play()
+    var audio = document.querySelector('audio');
+    var audioButton = document.getElementById('audioButton');
+    audio.loop = true;
+
+    console.log(audioButton);
+
+    if (audio.paused) {
+        audioButton.style.backgroundImage = "url('assets/img/play.png')";
+        audio.play();
+        return;
+    }
+    
+    audioButton.style.backgroundImage = "url('assets/img/mute.png')";
+    audio.pause();
 }
